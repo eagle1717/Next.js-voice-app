919eebeb45511a341c74b4a681a13d88178fecc1
Tamerlan Musayev <62748437+eagle1717@users.noreply.github.com>
2022-06-12 10:36:17 +0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
 import { useEffect } from "react"
 import DefaultLayout from "../layouts/default";
 import "bootstrap/dist/css/bootstrap.css";
+import axios from "axios"
+
+axios.defaults.baseURL = `${window.location.origin}/`
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
